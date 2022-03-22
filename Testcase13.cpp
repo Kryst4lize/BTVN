e5aa727118de8b0f14c25ecaf87955ab08957e58
Lê Thanh Minh <lethanhminh0801@gmail.com>
2022-03-23 03:48:29 +0700

    23/03/2022

--- Testcase13.cpp
+++ Testcase13.cpp
@@ -24,59 +24,51 @@
 #define ll long long 
 using namespace std;
 int main() {
-	ll n, m;
-	cin >> n >> m;
-	vector<ll>arr(n);
-	for (int i = 0; i < n; i++) {
+	int m, n;
+	cin >> m >> n;
+	vector<int>arr(n + 1);
+	vector<int>sum(n + 1);
+	for (int i = 1; i <= n; i++) {
 		cin >> arr[i];
 	}
-	ll res2;
-	ll end = (long long)1e16, start = 0;
-	while (start <= end) {
-		ll res = (start + end) >> 1;
-		//cout << start << " " << res << " " << end << endl;
-		ll count = 0, i = 0;
-		vector<ll>chapter(m);
-		for (; i < n && count < m; i++) {
-			if (chapter[count] == 0) {
-				if (chapter[count] + arr[i] > res) {
-					start = res;
-					break;
-				}
-				else {
-					chapter[count] += arr[i];
-				}
-			}
-			else {
-				if (chapter[count] + arr[i] > res) {
-					count++;
-					i--;
-				}
-				else {
-					chapter[count] += arr[i];
-				}
-			}
-		}
-		/*for (auto a : chapter) {
-				cout << a << " ";
-			}
-			cout << endl;*/
-			//5 3
-
-			//3 7 12 8 5
-		if (i == n) {
-			if (count > m - (long long)1) {
-				start = res + (long long)1;
-			}
-			else {
-				res2 = res;
-				end = res - (long long)1;
-			}
+	for (int i = 1; i <= n; i++) {
+		if (i != 1) {
+			sum[i] += sum[i - 1] + arr[i] + 1;
 		}
 		else {
-			//can it so de hoanf thanh n chuong
-			start = res + 1;
+			sum[i] += sum[i - 1] + arr[i];
 		}
 	}
-	cout << res2;
+	vector<vector<int>>dp(n + 1);
+	for (int i = 0; i <= n; i++) {
+		dp[i].resize(m + 2);
+	}
+	dp[0][0] = 1;
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < m + 1; j++) {
+
+				if (arr[i] + j > m) {
+					dp[i][j] = 0;
+				}
+				else {
+					for (int k = sum[i-1]; k < j; k++) {
+						dp[i][j] += dp[i - 1][k];
+					}
+				}
+
+			
+		}
+	}
+	int res = 0;
+	for (auto a : dp) {
+		for (auto b : a) {
+			cout << b << " ";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < m+2; i++) {
+		res += dp[n][i];
+	}
+	cout << res;
+	return 0;
 }
