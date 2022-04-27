7c7ec5e435abb2c2b49888ea46e8f5729926c5e0
Lê Thanh Minh <lethanhminh0801@gmail.com>
2022-04-27 22:38:38 +0700

    11/05/2022

---
+++ Testcase17.cpp
@@ -0,0 +1,36 @@
+#include<string>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<set>
+#include<stack>
+#include<map>
+#include<queue>
+#include<deque>
+#include<string>
+#include<iomanip>
+#include<cmath>
+#include<iostream>
+#include<algorithm>
+#include<climits>
+#include<bitset>
+#include<fstream>
+#include<chrono>
+#include<functional>
+#include<utility>
+#include<limits>
+#include<vector>
+#include<random>
+#include<sstream>
+#include<tuple>
+#include<fstream>
+using namespace std::chrono;
+using namespace std;
+#define mod 2147483647
+#define ll long long
+#define base1 256
+#define base2 256
+#define base3 7
+int mainte17() {
+	return 0;
+}
